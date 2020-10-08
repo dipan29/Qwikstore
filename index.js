8c13af5b4d2c8e4b187f4b0c7ef565d226f874e7
Dipan Roy <38533808+dipan29@users.noreply.github.com>
2020-10-08 13:25:09 +0530

    Models

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 // const basicAuth = require('express-basic-auth');
 const cron = require('node-cron');
+require('dotenv').config();
 
 const port = process.env.PORT || 3000;
 
---
+++ models/Values.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const values = new Schema({
+    id: {
+        type: String,
+        unique: true
+    },
+    name: {
+        // Stores the Variable Unique Name
+        type: String,
+        required: true,
+        unique: true
+    },
+    type: {
+        // Stores the Variable Type
+        type: String,
+        required: true
+    },
+    name: {
+        // Stores the Value - Any Type
+        type: Schema.Types.Mixed,
+        required: true
+    },
+    api_key: {
+        type: String,
+        required: true
+    }
+}, {timestamps: true});
+
+const Values = mongoose.model('Values', values);
+
+module.exports = Values;
